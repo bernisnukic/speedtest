068c2ae2ad4f69cf51814449f55932758c880981
exwizzard <44529204+bernisnukic@users.noreply.github.com>
2019-08-03 22:00:59 +0200

    wip

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ document.getElementById('download').onclick = function () {
 
     output.innerHTML = "Starting download..."
 
-    axios.get('https://speedtest.bernis.dev/speedtest/down')
+    axios.get('https://speedtest.bernis.dev/speedtest/down?bytes=100000000')
         .then(function (res) {
             //output.className = 'container';
             //output.innerHTML = res.data;
